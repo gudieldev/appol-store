a5920c70808e56b1c4b6cb4a60e3174be868895c
gudieldev <gudieldev@gmail.com>
2022-01-06 12:34:52 -0600

    form variants

--- assets/site-scripts.js
+++ assets/site-scripts.js
@@ -3,7 +3,7 @@ let productButtons = document.querySelectorAll('.modal-product-btn');
 
 productButtons.forEach(button => {
 	button.addEventListener('click', () => {
-		let url = '/products/' + button.getAttribute('data-product-handle') + '.js';
+		let url = '/products/' + button.getAttribute('data-product-handle') + '.js?currency=GTQ';
 		fetch( url )
 		.then( response => response.json() )
 		.then( (product_data) => {
@@ -11,6 +11,15 @@ productButtons.forEach(button => {
 			document.querySelector('.modal-product-details h3').innerText = product_data.title;
 			document.querySelector('.modal-product-details h5 strong').innerText = button.getAttribute('data-product-price');
 			document.querySelector('.modal-product-details p').innerHTML = product_data.description;
+
+			let variants = product_data.variants;
+			console.log(product_data);
+			let variantsSelect = document.querySelector('.modal-product-variants');
+			variantsSelect.innerHTML = '';
+
+			variants.forEach( ( variant, index ) => {
+				variantsSelect.options[variantsSelect.options.length] = new Option(variant.name, variant.id);
+			});
 			productModal.show();
 		})
 	});
